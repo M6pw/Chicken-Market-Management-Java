61f1a98dbd7936d5c4305f56fffc38a4a23a88ef
Masha Al_Dossari <6zz75fc2dt@privaterelay.appleid.com>
2026-07-18 06:34:15 +0300

    Add files via upload

---
+++ Chicken-Market-Management-Java.java
@@ -0,0 +1,97 @@
+package chickenmarket;
+
+import java.util.Scanner;
+
+public class ChickenMarketApp {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter chicken ID: ");
+        int id = input.nextInt();
+
+        System.out.print("Enter chicken weight: ");
+        double weight = input.nextDouble();
+
+        Chicken ch = new Chicken(id, weight);
+
+        System.out.print("Enter daily price: ");
+        double dailyPrice = input.nextDouble();
+        Chicken.setDailyPrice(dailyPrice);
+
+        System.out.print("Enter necessary weight: ");
+        double necessaryWeight = input.nextDouble();
+        Chicken.setNecessaryWeight(necessaryWeight);
+
+        ch.print();
+
+        input.close();
+    }
+}
+
+
+class Chicken {
+
+    private static double dailyPrice = 5;
+    private static double necessaryWeight = 1.2;
+
+    private double weight;
+    private int id;
+
+
+    public Chicken(int id, double weight) {
+        this.weight = weight;
+        this.id = id;
+    }
+
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+
+    public static void setDailyPrice(double newDailyPrice) {
+        dailyPrice = newDailyPrice;
+    }
+
+
+    public static void setNecessaryWeight(double newNecessaryWeight) {
+        necessaryWeight = newNecessaryWeight;
+    }
+
+
+    public boolean canMarket() {
+        return weight >= necessaryWeight;
+    }
+
+
+    public double price() {
+
+        if (canMarket()) {
+            return weight * dailyPrice;
+        }
+
+        return 0;
+    }
+
+
+    public void print() {
+
+        System.out.println("\n----- Chicken Information -----");
+        System.out.println("Chicken Weight: " + weight);
+        System.out.println("Chicken ID: " + id);
+
+        if (canMarket()) {
+            System.out.println("Ready for market");
+            System.out.println("Price: " + price());
+        } else {
+            System.out.println("Does not meet the necessary weight");
+        }
+    }
+}
